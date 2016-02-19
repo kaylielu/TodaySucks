545c9927772f0ad7b5f6652541686d84c168397b
kaylielu <kaylielu8@gmail.com>
2016-02-19 02:08:01 -0800

    Create RecordMemoryActivity.java
    Records memory

---
+++ RecordMemoryActivity.java
@@ -0,0 +1,42 @@
+package com.example.kaylie.todaysucks;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.content.Intent;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.RelativeLayout;
+
+public class RecordMemoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_memory);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        Intent intent = getIntent();
+        String memory = intent.getStringExtra(Choose.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(memory);
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.content);
+        layout.addView(textView);
+    }
+
+
+
+
+
+
+
+}
+
